98a8dd3245188fe6ee66d1496d9c2751f14c2390
SaeyonSivakumaran <saeyon2000@gmail.com>
2019-05-16 12:37:24 -0400

    Implemented access to JSON data from portals

--- DatabaseWebApp/Controllers/ZohoAPIController.cs
+++ DatabaseWebApp/Controllers/ZohoAPIController.cs
@@ -16,6 +16,7 @@ namespace DatabaseWebApp.Controllers
         public async Task<ActionResult> Index(string code)
         {
             ViewData["displayWeather"] = true;
+            ViewData["invalidscope"] = false;
             ApiHelper.InitializeClient();
 
             // Try to load the weather and if not possible, do not show the weather widget
@@ -39,11 +40,11 @@ namespace DatabaseWebApp.Controllers
                 JObject authJson = JObject.Parse(authInfo);
                 string accessToken = authJson["access_token"].ToString();
                 string refreshToken = authJson["refresh_token"].ToString();
-                string zohoJson = await GetZohoData(accessToken);
-                return Content(zohoJson);
-                //return RedirectToAction("ZohoContent", new { jsonData = zohoJson });
-            }
-            else
+                return await GetZohoData(accessToken);
+            } else if (code == "invalid")
+            {
+                ViewData["invalidscope"] = true;
+            } else
             {
                 ViewData["displayCode"] = false;
             }
@@ -65,9 +66,9 @@ namespace DatabaseWebApp.Controllers
         {
             // Redirecting to authorization request url
             string clientId = "1000.6HOB8P0UQQ093190608HV63TWPQ6AH";
-            string scope = formInputs["scope"];
+            Session["scope"] = formInputs["scope"];
             string redirect = "http://testwebappassetsoft.azurewebsites.net/ZohoAPI/Index";
-            string redirectUrl = $"https://accounts.zoho.com/oauth/v2/auth?scope={ scope }&client_id={ clientId }&response_type=code&access_type=offline&redirect_uri={ redirect }&prompt=consent";
+            string redirectUrl = $"https://accounts.zoho.com/oauth/v2/auth?scope={ Session["scope"] }&client_id={ clientId }&response_type=code&access_type=offline&redirect_uri={ redirect }&prompt=consent";
             return Redirect(redirectUrl);
         }
 
@@ -83,21 +84,34 @@ namespace DatabaseWebApp.Controllers
             return resultContent;
         }
 
-        public async Task<String> GetZohoData(string access)
+        public async Task<ActionResult> GetZohoData(string access)
         {
             // Send a GET request to Zoho with an authorization header
             using (var client = new HttpClient())
             {
-                string url = "https://projectsapi.zoho.com/restapi/portals/";
-                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + access);
-                var response = await client.GetStringAsync(url);
-                return response;
+                // Using the scope to assign a valid matching url if possible
+                string url = "";
+                if (Session["scope"].ToString() == "ZohoProjects.portals.READ")
+                {
+                    url = "https://projectsapi.zoho.com/restapi/portals/";
+                }
+
+                // Redirecting to the appropriate action if there is a valid url
+                if (!String.IsNullOrEmpty(url))
+                {
+                    client.DefaultRequestHeaders.Add("Authorization", "Bearer " + access);
+                    var response = await client.GetStringAsync(url);
+                    return ZohoContent(response);
+                } else
+                {
+                    return RedirectToAction("Index", new { code = "invalid" });
+                }
             }
         }
 
         public ActionResult ZohoContent(string jsonData)
         {
-            return Content("");
+            return Content(jsonData);
         }
     }
 }
