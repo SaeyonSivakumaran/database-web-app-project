211c5ebcfecd0fda72588aae11fbd31431ff3a01
SaeyonSivakumaran <saeyon2000@gmail.com>
2019-05-16 10:12:58 -0400

    More OAuth functionality

--- DatabaseWebApp/Controllers/ZohoAPIController.cs
+++ DatabaseWebApp/Controllers/ZohoAPIController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using DatabaseWebApp.Models;
 using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace DatabaseWebApp.Controllers
 {
@@ -25,6 +26,8 @@ namespace DatabaseWebApp.Controllers
             {
                 ViewData["displayCode"] = true;
                 ViewData["code"] = code;
+                string authInfo = await this.OAuthPost(code);
+                return Content(authInfo);
             }
             else
             {
@@ -45,11 +48,25 @@ namespace DatabaseWebApp.Controllers
 
         public ActionResult OAuthRedirect(FormCollection formInputs)
         {
-            string clientId = formInputs["clientID"];
+            string clientId = "1000.6HOB8P0UQQ093190608HV63TWPQ6AH";
             string scope = formInputs["scope"];
             string redirect = "http://testwebappassetsoft.azurewebsites.net/ZohoAPI/Index";
             string redirectUrl = $"https://accounts.zoho.com/oauth/v2/auth?scope={ scope }&client_id={ clientId }&response_type=code&access_type=offline&redirect_uri={ redirect }&prompt=consent";
             return Redirect(redirectUrl);
+        }
+
+        public async Task<String> OAuthPost(string token)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string clientId = "1000.6HOB8P0UQQ093190608HV63TWPQ6AH";
+                string clientSecret = "0ecfecbf51808d735e7d4c571da64b3370fe03f50d";
+                string redirect = "http://testwebappassetsoft.azurewebsites.net/ZohoAPI/Index";
+                string url = $"https://accounts.zoho.com/oauth/v2/token?code={ token }&redirect_uri={ redirect }&client_id={ clientId }&client_secret ={ clientSecret } &grant_type = authorization_code";
+                var post = await client.PostAsync(url, null);
+                string resultContent = await post.Content.ReadAsStringAsync();
+                return resultContent;
+            }
         }
     }
 }
